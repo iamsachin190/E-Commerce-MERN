8b16eaa442608a44e5c67822bf4e5d08fe39dffc
Sachin Sadh <117935531+iamsachin190@users.noreply.github.com>
2024-12-21 23:03:48 +0530

    Update index.js
    deleting url

--- backend/src/db/index.js
+++ backend/src/db/index.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-        const connectionInstance = await mongoose.connect("mongodb+srv://sachinsadh:452366@apnaguru.wr82olg.mongodb.net/apnaguru")
+        const connectionInstance = await mongoose.connect("Private Credentials ")
         console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
     } catch (error) {
         console.log("MONGODB connection FAILED ", error);
